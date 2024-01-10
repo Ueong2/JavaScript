bbc4e8bbf3e174cba29b365b1d4ad4091172ae27
JHK <kiko130@naver.com>
2024-01-10 11:01:50 +0900

    datalist, select의 option을 통한 사진 변경

--- javascript1-dom.js
+++ javascript1-dom.js
@@ -42,7 +42,7 @@ window.addEventListener("load", function(){
 // Ex3 : Selectors API Level1
 window.addEventListener("load", function(){
     var section3 = document.getElementById("section3");
-    // .은 태그의 class #은 태그의 id
+    // querySelector에서 .은 태그의 class #은 태그의 id
     // var txtX = section3.querySelector(".txt-x");
     var txtX = section3.querySelector("input[name='x']"); // class 대신 name 인 경우
     var txtY = section3.querySelector(".txt-y");
@@ -54,5 +54,36 @@ window.addEventListener("load", function(){
         var y = parseInt(txtY.value);
 
         txtSum.value = x+y;
+    };
+});
+
+
+// Ex4 : childeNodes를 이용한 노드 선택
+window.addEventListener("load", function(){
+    var section4 = document.querySelector("#section4");
+
+    var box = section4.querySelector(".box");
+
+    var input1 = box.children[0]; // childNodes[0];
+    var input2 = box.children[1]; // childNodes[1];
+    // childNodes로 선택 시 okay만 출력되는데 이는 html의 div와 input 
+    // 사이의 공백 또한 Node로 인식하기 때문이다.
+
+    input1.value = "hello";
+    input2.value = "okay";
+});
+
+// Ex5 : 엘리먼트 노드의 속성 변경
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section5");
+
+    var srcInput = section.querySelector(".src-input");
+    var imgSelect = section.querySelector(".img-select");
+    var changeButton = section.querySelector(".change-button");
+    var img = section.querySelector(".img");
+
+    changeButton.onclick = function(){
+        img.src = "images/" + srcInput.value;
+        // img.src = "images/" + imgSelect.value;
     };
 });
